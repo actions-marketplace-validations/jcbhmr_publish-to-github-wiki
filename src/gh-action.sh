005c861c9102f3baf57ad4647846ce699e2d88ea
Jacob Hummer <jcbhmr@outlook.com>
2023-01-20 14:50:40 -0600

    Hopefully make it better

--- src/gh-action.sh
+++ src/gh-action.sh
@@ -1,26 +1,12 @@
 #!/bin/bash -e
-# 1. Copy the folder to a temp folder.
-# 2. Commit everything!
-# 3. Force push to the remote .wiki.git repo on GitHub.
-# 4. Set the output page_url variable to the wiki URL
 
-# 1.
-temp_dir="$(mktemp -d)"
-cp -vr "$INPUT_PATH" "$temp_dir"
-cd "$temp_dir/$INPUT_PATH"
+ref="$(git subtree split -P "$INPUT_PATH")"
+git push -f github-wiki "$ref:master"
 
-# 2.
-git init -b master
-# https://github.com/stefanzweifel/git-auto-commit-action/blob/master/action.yml
-git config --local user.name "github-actions[bot]"
-git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
-git add -A
-git commit -m jcbhmr/deploy-wiki
-
-# 3.
-# GitHub auth is user:token@github.com, not token@github.com!
-git remote add wiki "https://$GITHUB_ACTOR:$GITHUB_TOKEN@github.com/$GITHUB_REPOSITORY.wiki.git"
-git push -f wiki master
-
-# 4.
-echo "page_url=https://github.com/$GITHUB_REPOSITORY/wiki" >> $GITHUB_OUTPUT
+# https://docs.github.com/en/actions/learn-github-actions/variables#default-environment-variables
+# GITHUB_SERVER_URL	The URL of the GitHub server. For example: https://github.com.
+# GITHUB_REPOSITORY	The owner and repository name. For example, octocat/Hello-World.
+#
+# https://docs.github.com/en/actions/using-jobs/defining-outputs-for-jobs
+# Example: echo "test=world" >> $GITHUB_OUTPUT
+echo "page_url=$GITHUB_SERVER_URL/$GITHUB_REPOSITORY/wiki" >> $GITHUB_OUTPUT
